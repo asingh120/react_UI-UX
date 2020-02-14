a77ebd589c1541835aa02b9cb9b5a39a33e8bdad
Amit Singh <asingh12015@gmail.com>
2020-02-13 22:58:24 -0500

    updated JS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react';
+import React from 'react';
 import Data from './Data/data';
 import Chart from './Components/chart';
 
@@ -6,16 +6,6 @@ import Chart from './Components/chart';
 import './App.css';
 
 const App = () => {
-
-
-    // const txData = require('./Data/data.json');
-    // const response = fetch(txData);
-    // const stuff = response.json;
-    // console.log(stuff);
- 
-
-  console.log("what the fuck");
-
   return (
     <div className="App">
       <h1>Account Summary</h1>
@@ -23,8 +13,6 @@ const App = () => {
         <li><Chart/></li>
         <li><Data/></li>
       </ul>
-
-
     </div>
   )
 }
--- src/Components/chart.js
+++ src/Components/chart.js
@@ -5,20 +5,19 @@ import { PieChart, Pie } from 'recharts';
 const data = TxData.transactions;
 
 const amount = data.map((dataDetail, index) => {
-    return (dataDetail.amount);
+    return (dataDetail.amount.amount);   
   });
 
 const type =  data.map((dataDetail, index) => {
     return (dataDetail.type);
   });
 
-const chartData = [data.amount];
-
 const Chart = () => { 
   return(
     <div>
       <h3>This is a test of PieChart</h3>
-      {console.log("this is " , chartData)}
+      {console.log("this is amount ", amount)}
+      {console.log("this is type " , type)}
       <PieChart width={730} height={250}>
         <Pie data={amount} dataKey="type" nameKey="amount" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
         <Pie data={type} dataKey="type" nameKey="amount" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
